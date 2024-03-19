49f2cc37d386daa683938abb8609dfc5fdd437c9
judahx67 <145267464+judahx67@users.noreply.github.com>
2024-03-19 22:17:42 +0700

    Add files via upload

--- ffmpeg_cropping_tool.bat
+++ ffmpeg_cropping_tool.bat
@@ -1,35 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSTk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAjk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal enabledelayedexpansion
 
@@ -40,22 +8,25 @@ REM Prompt for output file name (including extension)
 set /p output_path=Enter the output file path: 
 
 REM Prompt for start time (format: hh:mm:ss.ms)
-set /p start_time=Enter the start time (e.g., 00:06:20.112): 
+set /p start_time_input=Enter the start time (e.g., 00:06:20.112 Leave empty for whole file): 
+set start_time =-ss %start_time_input%
+if "%start_time_input%" == "" set start_time =
 
 REM Prompt for end time (format: hh:mm:ss.ms)
-set /p end_time=Enter the end time (e.g., 00:06:30.000): 
-
+set /p end_time_input=Enter the end time (e.g., 00:06:30.000 Leave empty for whole file): 
+set end_time =-to %end_time_input%
+if "%end_time_input%" == "" set end_time =
 REM Prompt for video CODEC (Matching file extension) 
-set /p video_codec_input=Enter video codec (e.g., h264_nvenc; h264; vp9; av01;... Leave empty to let FFmpeg choose)
+set /p video_codec_input=Enter video codec (e.g., h264_nvenc; h264; vp9; av01;... Leave empty to let FFmpeg choose): 
 
 set /p video_preset_input=Enter video preset (leave empty to let FFmpeg choose): 
 
 REM If there's no input, does not include -c:v in the command itself
 REM If there's no preset input, does not include -preset in the command itself
-if "%video_codec_input%" == "" set video_codec=^@
+if "%video_codec_input%" == "" set video_codec=
 if "%video_codec_input%" neq "" set video_codec=-c:v %video_codec_input%
 
-if "%video_preset_input%" == "" set video_preset=^@
+if "%video_preset_input%" == "" set video_preset=
 if "%video_preset_input%" neq "" set video_preset=-preset %video_preset_input%
 
 REM Prompt for video bitrate (default: 16000k)
@@ -67,7 +38,7 @@ set /p audio_bitrate=Enter the audio bitrate (default: 320k):
 if "%audio_bitrate%" == "" set "audio_bitrate=320k"
 
 REM Construct the FFmpeg command
-set ffmpeg_cmd=ffmpeg -ss %start_time% -to %end_time% -i !input_path! %video_codec% %video_preset% -b:v %video_bitrate% -b:a %audio_bitrate% !output_path!
+set ffmpeg_cmd=ffmpeg %start_time% %end_time% -i !input_path! %video_codec% %video_preset% -b:v %video_bitrate% -b:a %audio_bitrate% !output_path!
 
 REM Display the FFmpeg command
 echo The following command will be executed:
